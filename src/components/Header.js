efbe1207141d9b9fb45c446aa49a3b02a7d95520
Sakshi Madrecha <jain.sakshi1099@gmail.com>
2021-03-25 21:37:38 +0530

    fetched data from load btn and search btn

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LoadData from './LoadData';
+
 
 class Header extends React.Component{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            data: [],
+            is_data_loaded: false
+        }
+
+        this.getData = this.getData.bind(this);
+        this.search=this.search.bind(this);
+    }
+
+
+    getData() {
+          this.setState({
+            is_data_loaded: true
+        })
+        var main_data = this;
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                var json = JSON.parse(this.responseText);
+
+                main_data.setState({
+                    data: main_data.state.data.concat(json)
+                })
+
+            }
+        }
+        xhttp.open("GET", "https://jobs.github.com/positions.json?utf-8=true", true);
+        xhttp.send();
+
+    }
+
+    search()
+    {
+        this.setState({
+            is_data_loaded: true
+        })
+        var main_data = this;
+        var title = document.getElementById("s-title").value.trim()
+        var location = document.getElementById("s-location").value.trim()
+        var fullTime = document.getElementById("s-full-time").checked
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                var json = JSON.parse(this.responseText);
+                main_data.setState({
+                    data: main_data.state.data.concat(json)
+                })
+
+            }
+        }
+        xhttp.open("GET", `https://jobs.github.com/positions.json?description=${title}&location=${location}&full_time=${fullTime}`, true);
+        xhttp.send();
+
+    }
+
+
     render(){
         return (
+            <React.Fragment>
                 <div>
                     <div className="header">
                         <div className="header-items">
@@ -19,15 +79,20 @@ class Header extends React.Component{
                             <div className="d"></div>
                             <input type="checkbox" id="s-full-time" name="s-full-time" />&nbsp;
                             <label for="s-full-time">Full Time only</label>
-                            <button type="button" id="search-button" onclick="searchFunc()">Search</button>
+                             <button type="button" className="search-button" onClick={this.search}>Search</button>
                         </div>
                       
                     </div>
-                   <LoadData />
+
+
+                <button type="button" className="load_btn" onClick={this.getData}>Load More</button>
+
                 </div>
+                </React.Fragment>
             );
     }
 }
+
 
 ReactDOM.render(<Header  />, document.getElementById('root'));
 export default Header;
